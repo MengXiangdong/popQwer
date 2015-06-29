fe0e3985e77f0cae7fba0a31e28dae6374e7a071
MengXiangdong <15552185789@163.com>
2015-06-29 18:34:23 +0800

    Initial Commit

---
+++ Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by ola on 15/6/29.
+//  Copyright (c) 2015年 mengxiangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git
+//
+//  Created by ola on 15/6/29.
+//  Copyright (c) 2015年 mengxiangdong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
